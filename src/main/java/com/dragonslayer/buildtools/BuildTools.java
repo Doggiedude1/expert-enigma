bc4d3bd71afacc3c18dfea8ab73dda2c4522de77
Doggiedude1 <eli.hoestje@gmail.com>
2025-06-15 15:16:41 -0500

    Renamed some folders and some files

--- src/main/java/com/dragonslayer/buildtools/BuildTools.java
+++ src/main/java/com/dragonslayer/buildtools/BuildTools.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.dragonslayer.buildtools;
 
 import org.slf4j.Logger;
 
@@ -35,8 +35,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(BuildTools.MODID)
+public class BuildTools
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public BuildTools(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -82,7 +82,7 @@ public class ExampleMod
         CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (BuildTools) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
