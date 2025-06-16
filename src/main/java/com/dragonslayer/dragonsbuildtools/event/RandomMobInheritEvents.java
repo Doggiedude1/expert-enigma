a5c337550d2f85a6a5b3f7e6173e719dbc722ade
Doggiedude1 <eli.hoestje@gmail.com>
2025-06-16 18:38:09 -0500

    Fixed

--- src/main/java/com/dragonslayer/dragonsbuildtools/event/RandomMobInheritEvents.java
+++ src/main/java/com/dragonslayer/dragonsbuildtools/event/RandomMobInheritEvents.java
@@ -11,16 +11,17 @@ import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
-import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.minecraft.nbt.CompoundTag;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Vector;
 import java.util.function.Predicate;
+
 /**
  * Randomly assigns the goals and attributes of another mob whenever a mob joins the world.
  */
@@ -56,19 +57,18 @@ public class RandomMobInheritEvents {
         EntityType<? extends Mob> randomType = MOB_TYPES.get(random.nextInt(MOB_TYPES.size()));
         Mob randomMob = randomType.create(event.getLevel());
         if (randomMob == null) return;
-        System.out.println(randomMob);
-        randomMob.setInvisible(false);
+
+        randomMob.setInvisible(true);
         randomMob.setInvulnerable(true);
         randomMob.setNoGravity(true);
         randomMob.setSilent(true);
-        Vec3 mobPos = new Vec3(mob.position().x,mob.position().y - 10, mob.position().z);
-        randomMob.moveTo(mobPos);
+        randomMob.moveTo(mob.position());
         randomMob.getPersistentData().putBoolean(SKIP_TAG, true);
         event.getLevel().addFreshEntity(randomMob);
 
         copyAttributes(mob, randomMob);
-        copyGoalSelector(mob.goalSelector, randomMob.goalSelector);
-        copyGoalSelector(mob.targetSelector, randomMob.targetSelector);
+        copyGoalSelector(mob, randomMob.goalSelector, randomMob);
+        copyGoalSelector(mob, randomMob.targetSelector, randomMob);
 
         randomMob.discard();
     }
@@ -83,13 +83,40 @@ public class RandomMobInheritEvents {
         }
     }
 
-    private static void copyGoalSelector(GoalSelector target, GoalSelector source) {
+    private static void copyGoalSelector(Mob newOwner, GoalSelector sourceSelector, Mob oldOwner) {
         Predicate<Goal> any = g -> true;
+        GoalSelector target = sourceSelector == oldOwner.goalSelector ? newOwner.goalSelector : newOwner.targetSelector;
         target.removeAllGoals(any);
-        for (WrappedGoal g : source.getAvailableGoals()) {
+        for (WrappedGoal g : sourceSelector.getAvailableGoals()) {
             Goal goal = g.getGoal();
+            retargetGoal(goal, oldOwner, newOwner);
             int priority = g.getPriority();
             target.addGoal(priority, goal);
+        }
+    }
+
+    private static void retargetGoal(Goal goal, Mob oldOwner, Mob newOwner) {
+        Class<?> cls = goal.getClass();
+        while (cls != null) {
+            for (Field f : cls.getDeclaredFields()) {
+                if (Modifier.isStatic(f.getModifiers())) continue;
+                Class<?> type = f.getType();
+                if (!Mob.class.isAssignableFrom(type)) continue;
+                f.setAccessible(true);
+                try {
+                    Object value = f.get(goal);
+                    if (value == oldOwner) {
+                        if (Modifier.isFinal(f.getModifiers())) {
+                            Field mods = Field.class.getDeclaredField("modifiers");
+                            mods.setAccessible(true);
+                            mods.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+                        }
+                        f.set(goal, newOwner);
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+            cls = cls.getSuperclass();
         }
     }
 }
