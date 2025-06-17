e4c331b0484ca8fd60816536da60bc667618010b
Doggiedude1 <eli.hoestje@gmail.com>
2025-06-17 18:41:37 -0500

    Fixed

--- src/main/java/com/dragonslayer/dragonsbuildtools/mixin/EntityScaleMixin.java
+++ src/main/java/com/dragonslayer/dragonsbuildtools/mixin/EntityScaleMixin.java
@@ -1,11 +1,11 @@
 package com.dragonslayer.dragonsbuildtools.mixin;
 
 import com.dragonslayer.dragonsbuildtools.accessor.ScaleAccessor;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyReturnValue;
 
 @Mixin(Entity.class)
 public abstract class EntityScaleMixin implements ScaleAccessor {
