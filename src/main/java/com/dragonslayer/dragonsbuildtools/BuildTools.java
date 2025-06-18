7721b4ff6b9a5c9b7b2c36c64b5bcd8b6fe810b7
Doggiedude1 <eli.hoestje@gmail.com>
2025-06-17 23:18:43 -0500

    Fix network registry crash

--- src/main/java/com/dragonslayer/dragonsbuildtools/BuildTools.java
+++ src/main/java/com/dragonslayer/dragonsbuildtools/BuildTools.java
@@ -49,6 +49,7 @@ public class BuildTools
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
+        // NetworkRegistry.setup is handled globally; calling it here caused a crash
     }
 
     // Add the example block item to the building blocks tab
